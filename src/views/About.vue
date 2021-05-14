04091727b7a5e1c391ce5911f130922d83d31f0d
Austin Bowdler <a.bowdler@live.com>
2021-05-14 12:15:51 -0500

    Working todo list

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, in alias?
+      Cumque quis vitae, veritatis perferendis cum ex excepturi animi blanditiis
+      quidem deserunt iusto doloremque laborum alias facilis quam nemo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, in alias?
+      Cumque quis vitae, veritatis perferendis cum ex excepturi animi blanditiis
+      quidem deserunt iusto doloremque laborum alias facilis quam nemo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, in alias?
+      Cumque quis vitae, veritatis perferendis cum ex excepturi animi blanditiis
+      quidem deserunt iusto doloremque laborum alias facilis quam nemo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, in alias?
+      Cumque quis vitae, veritatis perferendis cum ex excepturi animi blanditiis
+      quidem deserunt iusto doloremque laborum alias facilis quam nemo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, in alias?
+      Cumque quis vitae, veritatis perferendis cum ex excepturi animi blanditiis
+      quidem deserunt iusto doloremque laborum alias facilis quam nemo.
+    </p>
   </div>
 </template>
